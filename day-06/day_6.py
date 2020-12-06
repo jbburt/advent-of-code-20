be392f884be73e3866ef8c61568fb9bf20fc0065
Joshua Burt <joshua.burt@yale.edu>
2020-12-06 09:41:10 -0500

    day 6

---
+++ day-06/day_6.py
@@ -0,0 +1,11 @@
+f = 'day-06/input.txt'
+
+with open(f, 'r') as fp:
+    data = fp.read().split("\n\n")
+
+# Problem 1
+print(sum(len(set(s.replace('\n', ''))) for s in data))
+
+# Problem 2
+forms = [x.split('\n') for x in data]
+print(sum([len(set.intersection(*map(set, f))) for f in forms]))
